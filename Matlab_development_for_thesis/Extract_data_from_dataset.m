46a0b1573a5fcd36bfbfc11527bf72c2d19f4f09
FabioTorello <fabio.torello@virgilio.it>
2019-12-06 09:18:45 +0100

    Now the Matlab code create a .xlsx file with the new dataset

--- Matlab_development_for_thesis/Extract_data_from_dataset.m
+++ Matlab_development_for_thesis/Extract_data_from_dataset.m
@@ -6,6 +6,6 @@ clear all
 close all
 
 %%Acquire Excel file in a table -- Dataset
-%Dataset = table2cell(readtable('test1.2.xlsx'));
 Dataset = readtable('test1.2.xlsx');
 [new_Dataset] = check_marker(Dataset);
+writetable(new_Dataset,'new_test1.2.xlsx');
--- Matlab_development_for_thesis/check_marker.m
+++ Matlab_development_for_thesis/check_marker.m
@@ -2,9 +2,6 @@ function [new_Dataset] = check_marker(Dataset)
  rows = size(Dataset,1);
  columns = size(Dataset,2);
  
-%  table={'H';'B';'C';'S'}; elem=[1;5;6;16];  % build lookup table
-
-%new_column = table(ones(rows,1), 'VariableNames',{'Table_Part'});
 new_column = table('Size',[rows 1],'VariableTypes',{'string'},'VariableNames',{'Table_Part'});
 column_five=Dataset(:,5);
 
@@ -84,80 +81,6 @@ new_column(ix,1)={'LEG_12_LONG_BAR_REAL_MARKER'};
 % find any row with 'LEG_12_SHORT_BAR_REAL_MARKER'
 ix=any(ismember(column_five{:,1},{'ar_marker_37';'ar_marker_38';'ar_marker_39';'ar_marker_40'}),2);
 new_column(ix,1)={'LEG_12_SHORT_BAR_REAL_MARKER'};
-
-%  for i=1:rows
-%      %%if statements to check the strings in the fifth column of the table
-%      if (strcmp(column_five(i,1)=='ar_marker_16'))
-%          new_column(i,1)={'WORLD_CENTER_REAL_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_42'))             
-%          new_column(i,1)={'WORLD_CENTER_FICTIVE_MARKER'};
-%         
-%      elseif (strcmp(column_five(i,1),'ar_marker_17'))
-%          new_column(i,1)={'WORLD_X_CENTER_REAL_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_43'))            
-%          new_column(i,1)={'WORLD_X_CENTER_FICTIVE_MARKER'};
-%         
-%      elseif (strcmp(column_five(i,1),'ar_marker_18'))
-%          new_column(i,1)={'WORLD_Y_CENTER_REAL_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_44'))             
-%          new_column(i,1)={'WORLD_Y_CENTER_FICTIVE_MARKER'};
-%      
-%      elseif (strcmp(column_five(i,1),'ar_marker_100'))
-%          new_column(i,1)={'LEG_0_FICTIVE_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_0')||...
-%              strcmp(column_five(i,1),'ar_marker_1')||...
-%              strcmp(column_five(i,1),'ar_marker_2')||...
-%              strcmp(column_five(i,1),'ar_marker_3'))             
-%          new_column(i,1)={'LEG_0_LONG_BAR_REAL_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_24')||...
-%              strcmp(column_five(i,1),'ar_marker_25')||...
-%              strcmp(column_five(i,1),'ar_marker_26')||...
-%              strcmp(column_five(i,1),'ar_marker_27'))             
-%          new_column(i,1)={'LEG_0_SHORT_BAR_REAL_MARKER'};
-%          
-%      elseif (strcmp(column_five(i,1),'ar_marker_104'))
-%          new_column(i,1)={'LEG_4_FICTIVE_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_4')||...
-%              strcmp(column_five(i,1),'ar_marker_5')||...
-%              strcmp(column_five(i,1),'ar_marker_6')||...
-%              strcmp(column_five(i,1),'ar_marker_7'))             
-%          new_column(i,1)={'LEG_4_LONG_BAR_REAL_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_33')||...
-%              strcmp(column_five(i,1),'ar_marker_34')||...
-%              strcmp(column_five(i,1),'ar_marker_35')||...
-%              strcmp(column_five(i,1),'ar_marker_36'))             
-%          new_column(i,1)='LEG_4_SHORT_BAR_REAL_MARKER';
-%          
-%      elseif (strcmp(column_five(i,1),'ar_marker_108'))
-%          new_column(i,1)={'LEG_8_FICTIVE_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_8')||...
-%              strcmp(column_five(i,1),'ar_marker_9')||...
-%              strcmp(column_five(i,1),'ar_marker_10')||...
-%              strcmp(column_five(i,1),'ar_marker_11'))             
-%          new_column(i,1)={'LEG_8_LONG_BAR_REAL_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_19')||...
-%              strcmp(column_five(i,1),'ar_marker_20')||...
-%              strcmp(column_five(i,1),'ar_marker_21')||...
-%              strcmp(column_five(i,1),'ar_marker_22'))             
-%          new_column(i,1)={'LEG_8_SHORT_BAR_REAL_MARKER'};
-%          
-%      elseif (strcmp(column_five(i,1),'ar_marker_112'))
-%          new_column(i,1)={'LEG_12_FICTIVE_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_12')||...
-%              strcmp(column_five(i,1),'ar_marker_13')||...
-%              strcmp(column_five(i,1),'ar_marker_14')||...
-%              strcmp(column_five(i,1),'ar_marker_15'))             
-%          new_column(i,1)={'LEG_12_LONG_BAR_REAL_MARKER'};
-%      elseif (strcmp(column_five(i,1),'ar_marker_37')||...
-%              strcmp(column_five(i,1),'ar_marker_38')||...
-%              strcmp(column_five(i,1),'ar_marker_39')||...
-%              strcmp(column_five(i,1),'ar_marker_40'))             
-%          new_column(i,1)={'LEG_12_SHORT_BAR_REAL_MARKER'};
-%          
-%      else
-%          new_column(i,1)={'WORLD'};
-%      end
-%  end 
 
  new_Dataset=horzcat(Dataset(:,1:5),new_column,Dataset(:,6:12));
 end
